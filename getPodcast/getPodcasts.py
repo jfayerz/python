31f751f6eef043903e76b75e7dc63835c753a2d8
Jonathan <jfayerz@icloud.com>
2018-09-28 15:30:59 -0400

    added .py and .json files

---
+++ getPodcast/getPodcasts.py
@@ -0,0 +1,210 @@
+# python script to get the most recent dollop mp3 from the rss feed
+
+import feedparser
+import json
+import urllib
+from mutagen.mp3 import MP3
+from mutagen.id3 import ID3NoHeaderError
+from mutagen.id3 import ID3, TIT2, TALB, TPE1, TPE2, COMM, USLT, TCOM, TCON, TDRC, TRCK, TPOS
+
+with open('podDictionary.json') as podcasts:
+	info = json.load(podcasts)
+
+for podcast in info:
+	d = feedparser.parse(podcast['rss'])
+	if podcast['episode_number'] == 'yes' and podcast['season_number'] == 'yes':
+		if podcast['host'] == 'libsyn':
+			url = d.entries[0].links[1].href.partition("?")[0].rstrip()
+			title = d.entries[0].title.rstrip()
+			episodeNum = d.entries[0].itunes_episode
+			seasonNum = d.entries[0].itunes_season
+			fileName = title + ".mp3"
+			urllib.request.urlretrieve (url,fileName)
+
+			try:
+				audio = ID3(fileName)
+			except ID3NoHeaderError:
+				print("Adding ID3 header")
+				audio = ID3()
+
+			audio.add(TIT2(encoding=3,text=title)) #add title
+			audio.add(TRCK(encoding=3,text=episodeNum)) #add track number
+			audio.add(TPOS(encoding=3,text=seasonNum)) #add season number
+			audio.add(TPE1(encoding=3,text=podcast['artist'])) #add artist
+			audio.add(TPE2(encoding=3,text=podcast['album_artist'])) #add album artist
+			audio.add(TALB(encoding=3,text=podcast['album'])) #add album
+			audio.save("/home/mcshoggoth/Downloads/" + fileName)
+		elif podcast['host'] == 'art19':
+			url = d.entries[0].links[0].href
+			title = d.entries[0].title.rstrip()
+			episodeNum = d.entries[0].itunes_episode
+			seasonNum = d.entries[0].itunes_season
+			fileName = title + ".mp3"
+			urllib.request.urlretrieve (url,fileName)
+
+			try:
+				audio = ID3(fileName)
+			except ID3NoHeaderError:
+				print("Adding ID3 header")
+				audio = ID3()
+
+			audio.add(TIT2(encoding=3,text=title)) #add title
+			audio.add(TRCK(encoding=3,text=episodeNum)) #add track number
+			audio.add(TPOS(encoding=3,text=seasonNum)) #add season number
+			audio.add(TPE1(encoding=3,text=podcast['artist'])) #add artist
+			audio.add(TPE2(encoding=3,text=podcast['album_artist'])) #add album artist
+			audio.add(TALB(encoding=3,text=podcast['album'])) #add album
+			audio.save("/home/mcshoggoth/Downloads/" + fileName)
+			"""
+			audio = eyed3.load(fileName)
+			audio.tag.title = title
+			audio.tag.track_num = episodeNum
+			audio.tag.disc_num = seasonNum
+			audio.tag.artist = podcast['artist']
+			audio.tag.album_artist = podcast['album_artist']
+			audio.tag.album = podcast['album']
+			audio.tag.save(version=(2,4,0))
+			"""
+		else:
+			url = d.entries[0].links[1].href
+			title = d.entries[0].title.rstrip()
+			episodeNum = d.entries[0].itunes_episode
+			seasonNum = d.entries[0].itunes_season
+			fileName = title + ".mp3"
+			urllib.request.urlretrieve (url,fileName)
+
+			try:
+				audio = ID3(fileName)
+			except ID3NoHeaderError:
+				print("Adding ID3 header")
+				audio = ID3()
+
+			audio.add(TIT2(encoding=3,text=title)) #add title
+			audio.add(TRCK(encoding=3,text=episodeNum)) #add track number
+			audio.add(TPOS(encoding=3,text=seasonNum)) #add season number
+			audio.add(TPE1(encoding=3,text=podcast['artist'])) #add artist
+			audio.add(TPE2(encoding=3,text=podcast['album_artist'])) #add album artist
+			audio.add(TALB(encoding=3,text=podcast['album'])) #add album
+			audio.save("/home/mcshoggoth/Downloads/" + fileName)
+			"""
+			audio = eyed3.load(fileName)
+			audio.tag.title = title   # none of this eyed3 stuff works if the mp3 file has tag "none"
+			audio.tag.track_num = episodeNum
+			audio.tag.disc_num = seasonNum
+			audio.tag.artist = podcast['artist']
+			audio.tag.album_artist = podcast['album_artist']
+			audio.tag.album = podcast['album']
+			audio.tag.save(version=(2,4,0))
+			"""
+	elif podcast['episode_number'] == 'yes' and podcast['season_number'] == 'no':
+		if podcast['host'] == 'libsyn':
+			url = d.entries[0].links[1].href.partition("?")[0].rstrip()
+			title = d.entries[0].title.rstrip()
+			episodeNum = d.entries[0].itunes_episode
+			fileName = title + ".mp3"
+			urllib.request.urlretrieve (url,fileName)
+
+			try:
+				audio = ID3(fileName)
+			except ID3NoHeaderError:
+				print("Adding ID3 header")
+				audio = ID3()
+
+			audio.add(TIT2(encoding=3,text=title)) #add title
+			audio.add(TRCK(encoding=3,text=episodeNum)) #add track number
+			audio.add(TPE1(encoding=3,text=podcast['artist'])) #add artist
+			audio.add(TPE2(encoding=3,text=podcast['album_artist'])) #add album artist
+			audio.add(TALB(encoding=3,text=podcast['album'])) #add album
+			audio.save("/home/mcshoggoth/Downloads/" + fileName)
+		elif podcast['host'] == 'art19':
+			url = d.entries[0].links[0].href
+			title = d.entries[0].title.rstrip()
+			episodeNum = d.entries[0].itunes_episode
+			fileName = title + ".mp3"
+			urllib.request.urlretrieve (url,fileName)
+
+			try:
+				audio = ID3(fileName)
+			except ID3NoHeaderError:
+				print("Adding ID3 header")
+				audio = ID3()
+
+			audio.add(TIT2(encoding=3,text=title)) #add title
+			audio.add(TRCK(encoding=3,text=episodeNum)) #add track number
+			audio.add(TPE1(encoding=3,text=podcast['artist'])) #add artist
+			audio.add(TPE2(encoding=3,text=podcast['album_artist'])) #add album artist
+			audio.add(TALB(encoding=3,text=podcast['album'])) #add album
+			audio.save("/home/mcshoggoth/Downloads/" + fileName)
+		else:
+			url = d.entries[0].links[1].href
+			title = d.entries[0].title.rstrip()
+			episodeNum = d.entries[0].itunes_episode
+			fileName = title + ".mp3"
+			urllib.request.urlretrieve (url,fileName)
+
+			try:
+				audio = ID3(fileName)
+			except ID3NoHeaderError:
+				print("Adding ID3 header")
+				audio = ID3()
+
+			audio.add(TIT2(encoding=3,text=title)) #add title
+			audio.add(TRCK(encoding=3,text=episodeNum)) #add track number
+			audio.add(TPE1(encoding=3,text=podcast['artist'])) #add artist
+			audio.add(TPE2(encoding=3,text=podcast['album_artist'])) #add album artist
+			audio.add(TALB(encoding=3,text=podcast['album'])) #add album
+			audio.save("/home/mcshoggoth/Downloads/" + fileName)
+	elif podcast['episode_number'] == 'no':
+		if podcast['host'] == 'libsyn':
+			url = d.entries[0].links[1].href.partition("?")[0].rstrip()
+			title = d.entries[0].title.rstrip()
+			fileName = title + ".mp3"
+			episodeNum = d.entries[0].title.partition(" ")[0].rstrip()
+			urllib.request.urlretrieve (url,fileName)
+
+			try:
+				audio = ID3(fileName)
+			except ID3NoHeaderError:
+				print("Adding ID3 header")
+				audio = ID3()
+
+			audio.add(TIT2(encoding=3,text=title)) #add title
+			audio.add(TRCK(encoding=3,text=episodeNum)) #add tracknumber
+			audio.add(TPE1(encoding=3,text=podcast['artist'])) #add artist
+			audio.add(TPE2(encoding=3,text=podcast['album_artist'])) #add album artist
+			audio.add(TALB(encoding=3,text=podcast['album'])) #add album
+			audio.save("/home/mcshoggoth/Downloads/" + fileName)
+		elif podcast['host'] == 'art19':
+			url = d.entries[0].links[0].href
+			title = d.entries[0].title.rstrip()
+			fileName = title + ".mp3"
+			urllib.request.urlretrieve (url,fileName)
+
+			try:
+				audio = ID3(fileName)
+			except ID3NoHeaderError:
+				print("Adding ID3 header")
+				audio = ID3()
+
+			audio.add(TIT2(encoding=3,text=title)) #add title
+			audio.add(TPE1(encoding=3,text=podcast['artist'])) #add artist
+			audio.add(TPE2(encoding=3,text=podcast['album_artist'])) #add album artist
+			audio.add(TALB(encoding=3,text=podcast['album'])) #add album
+			audio.save("/home/mcshoggoth/Downloads/" + fileName)
+		else:
+			url = d.entries[0].links[1].href
+			title = d.entries[0].title.rstrip()
+			fileName = title + ".mp3"
+			urllib.request.urlretrieve (url,fileName)
+
+			try:
+				audio = ID3(fileName)
+			except ID3NoHeaderError:
+				print("Adding ID3 header")
+				audio = ID3()
+
+			audio.add(TIT2(encoding=3,text=title)) #add title
+			audio.add(TPE1(encoding=3,text=podcast['artist'])) #add artist
+			audio.add(TPE2(encoding=3,text=podcast['album_artist'])) #add album artist
+			audio.add(TALB(encoding=3,text=podcast['album'])) #add album
+			audio.save("/home/mcshoggoth/Downloads/" + fileName)
